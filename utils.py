f856161d9fb6208f70a1ab80bab62b0d21362651
effervescent000 <kislio01@gmail.com>
2021-06-21 15:42:32 -0400

    Docstrings + moved the application of haste to spells to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 def round_to_base(num, base):
+    """Round num to a multiplier of base."""
     return base * round(num / base)
 
 
@@ -12,3 +13,8 @@ def convert_spell_crit(amt):
 
 def convert_spell_hit(amt):
     return amt / 12.6 / 100
+
+
+def haste_spell(time, time_inc, haste):
+    """Apply haste % to a casted spell (or the GCD)"""
+    return round_to_base(time / (1 + haste), time_inc)
